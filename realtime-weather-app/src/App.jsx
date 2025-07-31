755c73b8c2ff1c7e1fad6bb6986d9362fe94804a
JoeJoe <iiispan.jojo@gmail.com>
2025-08-01 00:07:06 +0800

    refactor: create a new file called "weatherCard" view file to which the main part of card is moving

--- realtime-weather-app/src/App.jsx
+++ realtime-weather-app/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "normalize.css";
 import styled from "@emotion/styled";
 import { ThemeProvider } from "@emotion/react";
-import RefreshIcon from "./images/refresh.svg?react";
-import AirFlowIcon from "./images/airFlow.svg?react";
-import RainIcon from "./images/rain.svg?react";
-import WeatherIcon from "./components/weatherIcon";
 import { getMoment } from "./utils/helpers";
+import WeatherCard from "./views/WeatherCard";
+
 
 const Container = styled.div`
   background-color: #ededed;
@@ -16,103 +14,6 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: ${({ theme: { boxShadow } }) => boxShadow};
-  background-color: ${({ theme: { backgroundColor } }) => backgroundColor};
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
-
-const Location = styled.div`
-  font-size: ${(prop) => (prop.theme === 11 ? "128px" : "28px")};
-  color: ${({ theme: { titleColor } }) => titleColor};
-  margin-bottom: 20px;
-`;
-
-const Description = styled.div`
-  font-size: 16px;
-  color: ${({ theme: { textColor } }) => textColor};
-  margin-bottom: 30px;
-`;
-
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Temperature = styled.div`
-  color: #757575;
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
-
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
-
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16px;
-  font-weight: 300;
-  color: ${({ theme: { textColor } }) => textColor};
-  margin-bottom: 20px;
-
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme: { textColor } }) => textColor};
-
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-//Q: SVG直接等於一類DOM嗎? 為何可以直接這樣寫?
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: ${({ theme: { textColor } }) => textColor};
-
-  svg {
-    width: 15px;
-    height: 15px;
-    margin-left: 10px;
-    cursor: pointer;
-    ${({ Loading }) =>
-      Loading ? "animation: rotate 1.5s infinite linear" : ""}
-  }
-
-  @keyframes rotate {
-    from {
-      transform: rotate(360deg);
-    }
-    to {
-      transform: rotate(0deg);
-    }
-  }
-`;
 
 const theme = {
   light: {
@@ -137,6 +38,7 @@ const theme = {
 const ContainerX = styled(Container)`
   background-color: ${({ theme: { backgroundColor } }) => backgroundColor};
 `;
+
 const auth = "CWA-6223B667-3D20-4930-B865-47C2562798D1";
 
 const fetchCurrentWeather = async () => {
@@ -206,49 +108,25 @@ const fetchWeatherForecast = async () => {
 };
 
 function App() {
-  ////test useCallback
-  //  //forTesting useCallback
-  //  if(ggg===20)
-  //  {
-  //    ggg = fetchMain
-  //    console.log('ggg初始化: ',ggg)
-  //  }
-  //  else
-  //  {
-  //   console.log('已給過ggg')
-  //   bbb = fetchMain
-  //   console.log('兩次渲染fetchMain參照相同嗎?', ggg === bbb )
-  //  }
 
   //lazy init for useState
   const [t, setT] = useState(()=>getMoment("臺北市"));
 
-  const [
-    {
-      locationName,
-      weatherCode,
-      windSpeed,
-      airTemperature,
-      description,
-      PoP,
-      ObTime,
-      isLoading,
-      comfortability,
-    },
-    setW,
-  ] = useState({
-    locationName: "",
-    weatherCode: "",
-    windSpeed: "",
-    airTemperature: "",
-    description: "",
-    PoP: 0,
-    ObTime: "上午 00：00",
-    isLoading: true,
-    comfortability: "",
-  }); 
+  const [weatherData,setW,]= useState({locationName: "",
+                                        weatherCode: "",
+                                        windSpeed: "",
+                                        airTemperature: "",
+                                        description: "",
+                                        PoP: 0,
+                                        ObTime: "上午 00：00",
+                                        isLoading: true,
+                                        comfortability: "", });
+                                        
+                                        
+                            
 
-  const fetchMain = useCallback(async () => {
+
+  const fetchMain = useCallback( async () => {
     setW((x) => ({ ...x, isLoading: true }));
 
     const [curW, wForecast] = await Promise.all([
@@ -269,38 +147,13 @@ function App() {
   return (
     <ThemeProvider theme={theme[t === "day" ? "light" : "dark"]}>
       <ContainerX>
-        <WeatherCard>
-          <Location>{locationName}</Location>
-          <Description>
-            {description} {comfortability}
-          </Description>
-          <CurrentWeather>
-            <Temperature>
-              {airTemperature}
-              <Celsius>°C</Celsius>
-            </Temperature>
-            <WeatherIcon weatherCode={weatherCode} moment={t} />
-          </CurrentWeather>
-          <AirFlow>
-            <AirFlowIcon />
-            {windSpeed} m/h{" "}
-          </AirFlow>
-          <Rain>
-            <RainIcon />
-            {PoP}%{" "}
-          </Rain>
-          <Refresh
-            onClick={() => {
-              fetchMain();
-              setT( x=> x === getMoment("臺北市")? x : getMoment("臺北市") );
-            }}
-            Loading={isLoading}
-          >
-            {" "}
-            最後觀測時間：{ObTime}
-            <RefreshIcon />{" "}
-          </Refresh>
-        </WeatherCard>
+        <WeatherCard 
+          weatherData={weatherData} 
+          fetchMain={fetchMain}
+          setT={setT}
+          t={t}
+          getMoment={getMoment}
+          />
       </ContainerX>
     </ThemeProvider>
   );
---
+++ realtime-weather-app/src/views/WeatherCard.jsx
@@ -0,0 +1,158 @@
+import styled from "@emotion/styled";
+import RefreshIcon from "../images/refresh.svg?react";
+import AirFlowIcon from "../images/airFlow.svg?react";
+import RainIcon from "../images/rain.svg?react";
+import WeatherIcon from "../components/weatherIcon";
+
+const WeatherCardWrapper = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: ${({ theme: { boxShadow } }) => boxShadow};
+  background-color: ${({ theme: { backgroundColor } }) => backgroundColor};
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+const Location = styled.div`
+  font-size: ${(prop) => (prop.theme === 11 ? "128px" : "28px")};
+  color: ${({ theme: { titleColor } }) => titleColor};
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: ${({ theme: { textColor } }) => textColor};
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 300;
+  color: ${({ theme: { textColor } }) => textColor};
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: ${({ theme: { textColor } }) => textColor};
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+//Q: SVG直接等於一類DOM嗎? 為何可以直接這樣寫?
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: ${({ theme: { textColor } }) => textColor};
+
+  svg {
+    width: 15px;
+    height: 15px;
+    margin-left: 10px;
+    cursor: pointer;
+    ${({ Loading }) =>
+      Loading ? "animation: rotate 1.5s infinite linear" : ""}
+  }
+
+  @keyframes rotate {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0deg);
+    }
+  }
+`;
+
+function WeatherCard({
+  weatherData: {
+    locationName,
+    weatherCode,
+    windSpeed,
+    airTemperature,
+    description,
+    PoP,
+    ObTime,
+    isLoading,
+    comfortability,
+  },
+  fetchMain,
+  setT,
+  t,
+  getMoment,
+}) {
+  return (
+    <WeatherCardWrapper>
+      <Location>{locationName}</Location>
+      <Description>
+        {description} {comfortability}
+      </Description>
+      <CurrentWeather>
+        <Temperature>
+          {airTemperature}
+          <Celsius>°C</Celsius>
+        </Temperature>
+        <WeatherIcon weatherCode={weatherCode} moment={t} />
+      </CurrentWeather>
+      <AirFlow>
+        <AirFlowIcon />
+        {windSpeed} m/h{" "}
+      </AirFlow>
+      <Rain>
+        <RainIcon />
+        {PoP}%{" "}
+      </Rain>
+      <Refresh
+        onClick={() => {
+          fetchMain();
+          setT((x) => (x === getMoment("臺北市") ? x : getMoment("臺北市")));
+        }}
+        Loading={isLoading}
+      >
+        {" "}
+        最後觀測時間：{ObTime}
+        <RefreshIcon />{" "}
+      </Refresh>
+    </WeatherCardWrapper>
+  );
+}
+
+export default WeatherCard;
