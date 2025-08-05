8478ebf2bcbddd5ce9a6f6fba4d05bbf6d8f67ed
jojo_yau <jojo_yau@JOJOYAU-PCW10B>
2025-08-05 18:04:11 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react_HookPractice_prjs/sw.js', { scope: '/react_HookPractice_prjs/' })})}
